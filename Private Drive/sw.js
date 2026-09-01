aa856fffab0fb07af240d36afa00bfea374c5307
taithai <55917846+taithai-tai@users.noreply.github.com>
2026-09-02 06:04:29 +0700

    feat: add private drive portal and device guides

---
+++ Private Drive/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'taithai-private-drive-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
